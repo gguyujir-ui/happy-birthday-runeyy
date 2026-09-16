9803db86da9350281f85fa5b34c1bb3acd284d81
gguyujir-ui <gguyujir@gmail.com>
2026-09-16 16:34:06 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -452,6 +452,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const scenes = [
       { el: document.querySelector('.collage'), w: 350 },
+      { el: document.querySelector('.journey-scene'), w: 480 },
       { el: document.querySelector('.moment-scene'), w: 380 },
       { el: document.querySelector('.playlist-scene'), w: 420 }
     ];
