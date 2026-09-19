008ebfcd825e90c0de09393f6c1edcc79ebc8573
gguyujir-ui <gguyujir@gmail.com>
2026-09-19 22:17:13 +0700

    Update EMAILJS public key in script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@
 // terkirim otomatis ke emailmu. Cara dapetin nilai-nilai ini
 // ada di README.md bagian "Setup kirim Wish ke email".
 // ---------------------------------------------------------
-const EMAILJS_PUBLIC_KEY  = 'PFwO6FnKLFMlIEynb';
+const EMAILJS_PUBLIC_KEY  = 'PFwO6FnKLFMllEynb';
 const EMAILJS_SERVICE_ID  = 'service_n8u03nr';
 const EMAILJS_TEMPLATE_ID = 'template_io97yer';
 
